13dcaed5ee3c03be685fed5ff7e31df4773bf09b
Camille Baldock <camille@camillebaldock.co.uk>
2015-02-13 16:13:38 +0000

    Store default database uri in environment

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql+pg8000://postgres:password@172.16.42.43:5432/register_data'
+    SQLALCHEMY_DATABASE_URI = os.environ['POSTGRES_REGISTER_DATABASE_URI'] 
 
 class DevelopmentConfig(Config):
     DEBUG = True
