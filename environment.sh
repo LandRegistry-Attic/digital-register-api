95ccbbdf37fadf9f33809c8a0b1b367d1b8feb98
Pheonnexx <pheonn@hotmail.co.uk>
2015-02-27 12:05:02 +0000

    adding initial elasticsearch info and endpoint

--- environment.sh
+++ environment.sh
@@ -5,3 +5,4 @@ export POSTGRES_HOST='172.16.42.43'
 export POSTGRES_PORT=5432
 export POSTGRES_DB='register_data'
 export LOGGING_CONFIG_FILE_PATH='logging_config.json'
+export ELASTIC_SEARCH_ENDPOINT='http://54.154.72.110:4200'
