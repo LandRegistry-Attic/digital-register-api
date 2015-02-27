ef59c8570805ef1f177e17d65e3cee46f8eeabbc
Camille Baldock <camille@camillebaldock.co.uk>
2015-02-27 10:23:26 +0000

    Better granularity for database config

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
 import os
 
+user = os.environ['POSTGRES_USER']
+password = os.environ['POSTGRES_PASSWORD']
+host = os.environ['POSTGRES_HOST']
+port = os.environ['POSTGRES_PORT']
+database = os.environ['POSTGRES_DB']
+sql_alchemy_uri = 'postgresql+pg8000://{0}:{1}@{2}:{3}/{4}'.format(user, password, host, port, database)
+
 CONFIG_DICT = {
     'DEBUG': False,
-    'SQLALCHEMY_DATABASE_URI': os.environ['POSTGRES_REGISTER_DATABASE_URI'],
+    'SQLALCHEMY_DATABASE_URI': sql_alchemy_uri,
 }
 
 settings = os.environ.get('SETTINGS')
