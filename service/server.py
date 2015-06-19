c2d44ac972ae664cacbb83a4a2f87d442f647c68
Camille Baldock <camille@camillebaldock.co.uk>
2015-06-19 15:52:55 +0100

    When field is missing result should drop to bottom of scoped search

--- service/server.py
+++ service/server.py
@@ -63,15 +63,15 @@ def healthcheck():
 def get_property_address(postcode):
     search = create_search('property_by_postcode_3')
     query = search.filter('term', postcode=postcode).sort(
-        'street_name',
-        'house_no',
-        'house_alpha',
-        'street_name_2',
-        'secondary_house_no',
-        'secondary_house_alpha',
-        'sub_building_no',
-        'sub_building_description',
-        'first_number_in_address_string'
+        {'street_name' : {'missing' : '_last'}},
+        {'house_no' : {'missing' : '_last'}},
+        {'house_alpha' : {'missing' : '_last'}},
+        {'street_name_2' : {'missing' : '_last'}},
+        {'secondary_house_no' : {'missing' : '_last'}},
+        {'secondary_house_alpha' : {'missing' : '_last'}},
+        {'sub_building_no' : {'missing' : '_last'}},
+        {'sub_building_description' : {'missing' : '_last'}},
+        {'first_number_in_address_string' : {'missing' : '_last'}}
     )
     return query.execute().hits
 
