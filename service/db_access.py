c697249b994b1951ff22a52b4aad5b9285b2094a
lgonczar <gonczar@gmail.com>
2015-06-29 13:33:14 +0000

    Add an endpoint for returning official copy data of a title

--- service/db_access.py
+++ service/db_access.py
@@ -1,13 +1,38 @@
 from sqlalchemy import false
+from sqlalchemy.orm.strategy_options import load_only, Load
 from service.models import TitleRegisterData
 
 
 # TODO: write integration tests
-def get_title_register(title_ref):
-    if title_ref:
+def get_title_register(title_number):
+    # TODO: trust our own code to do the right thing - validate data on input instead
+    if title_number:
         # Will retrieve the first matching title that is not marked as deleted
-        result = TitleRegisterData.query.filter(TitleRegisterData.title_number == title_ref,
-                                                TitleRegisterData.is_deleted == false()).first()
+        result = TitleRegisterData.query.options(
+            Load(TitleRegisterData).load_only(
+                TitleRegisterData.title_number.name,
+                TitleRegisterData.register_data.name,
+                TitleRegisterData.geometry_data.name
+            )
+        ).filter(
+            TitleRegisterData.title_number == title_number,
+            TitleRegisterData.is_deleted == false()
+        ).first()
+
         return result
     else:
         raise TypeError('Title number must not be None.')
+
+
+def get_official_copy_data(title_number):
+    result = TitleRegisterData.query.options(
+        Load(TitleRegisterData).load_only(
+            TitleRegisterData.title_number.name,
+            TitleRegisterData.official_copy_data.name
+        )
+    ).filter(
+        TitleRegisterData.title_number == title_number,
+        TitleRegisterData.is_deleted == false()
+    ).first()
+
+    return result
